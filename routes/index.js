d6e7a4dced9ee042850e87fcac316b149b182a7c
shubhranshuthakur24 <shubhranshuthakur24@gmail.com>
2020-02-22 17:36:37 +0530

    added landing page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //welcome page
-router.get('/', (req, res) => res.send('welcome'));
+router.get('/', (req, res) => res.render('index.html'));
 
 
 module.exports = router;
